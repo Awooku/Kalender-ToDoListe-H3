dea648d2e66630d09db1c877317ac4b462cfe84e
Awooku <31002082+Awooku@users.noreply.github.com>
2019-08-22 12:07:49 +0200

    pls virk pls virk

---
+++ H3EventKalender/H3EventKalender/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace H3EventKalender.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ H3EventKalender/H3EventKalender/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(H3EventKalender.Startup))]
+
+namespace H3EventKalender
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
